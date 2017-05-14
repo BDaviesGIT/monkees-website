29ac3507be9b0f6fcb7345efdb2321cf024cb637
Ben <ben.davies27@outlook.com>
2017-05-14 20:38:15 +0100

    functionality completed

---
+++ register.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>The Monkees Website</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+ <style>
+  
+  body{
+  background-image:url('images/background.jpg');
+  }
+
+  </style>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  </head>
+  <body>
+  <!--Main Container-->
+  <div class="row">
+    <div class="col-sm-12">
+
+  <!--header image-->
+    <div class="container-fluid">
+      <div class="row">
+      <img src="images/header2.png" class="center-block img-responsive">
+      </div>
+    </div>
+
+
+
+    <!--Navigation Bar code-->
+
+  <div id="MainNav" class="navbar navbar-default" role="Navigation"> 
+      <div class="container-fluid">
+          <div class="Navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+               <span class="icon-bar"></span>
+              </button>
+          </div>
+          <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav center-block">
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="Discography.html">Discography</a></li>
+                  <li><a href="Media-video.html">Media</a></li>
+                  <li><a href="Tour.html">Tour</a></li>
+                  <li><a href="Bookings.html">Bookings</a></li>
+                  <li><a href="http://monkeesstore.warnermusic.com/music.html">Shop</a></li>
+                 
+                  <li><a href="https://www.facebook.com/TheMonkees/">
+                  <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i></i></a></li>
+                  
+                  <li><a href="https://twitter.com/themonkees?lang=en">
+                  <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a></li>
+                 
+                  <li><a href="https://www.youtube.com/user/themonkees">
+                  <i class="fa fa-youtube-square fa-2x" aria-hidden="true"></i></a></li>
+                </ul>
+          </div>
+      </div>
+  </div>
+
+
+    <script src='http://code.jquery.com/jquery.min.js'></script>
+    <script src='js/bootstrap.min.js'></script>
+
+    <div class='container'>
+     <div class='col-md-12'>
+                <h1>Booking</h1>
+                <p>
+                    Want to book the Monkees to play at your event? Fill in the below form, submit and one of our event team staff will be in touch within 48 hours to confirm the details of the booking and let you know if we are available.
+                </p>
+        <div class='row'>
+            <div class='col-md-6'>
+             <img src="images/good.jpg" alt="Good Times Album art" class="img-responsive"/>
+            </div>
+
+            <div class='col-md-6'>        
+            <h3 align="center-block">Your details have been submitted. Thanks!</h3>         
+
+        </div>
+</div>
+
+        <footer class="col-12">
+          <h5>Footer</h5>
+          <p>This website has been produced as a coursework assignment for part of the "Code Institute" online curriculum. I claim no ownership over any of the images, music or videos used as part of this website, which exists for educational purposes only.</p>
+        </footer>
+    </div>
+
+</body>
+</html>
